80052c202bc723945488fcb3746e24f066ce66be
dburnham1212 <dburnham1212@gmail.com>
2023-06-07 15:11:20 -0400

    added wasd movement in input.js

--- input.js
+++ input.js
@@ -1,11 +1,9 @@
 // setup interface to handle user input from stdin
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,6 +11,25 @@ const setupInput = function () {
   // on any input from stdin (standard input), output a "." to stdout
   stdin.on("data", handleUserInput);
   return stdin;
+};
+
+const handleUserInput = function (key) {
+  if(key === "w"){
+    console.log("w key pressed");
+    connection.write("Move: up");// send movement to server
+  } else if(key === "a"){
+    console.log("a key pressed");
+    connection.write("Move: left");// send movement to server
+  } else if(key === "s"){
+    console.log("s key pressed");
+    connection.write("Move: down");// send movement to server
+  } else if(key === "d"){
+    console.log("d key pressed");
+    connection.write("Move: right");// send movement to server
+  }
+  if (key === '\u0003') {
+    process.exit();
+  }
 };
 
 module.exports = { setupInput };
